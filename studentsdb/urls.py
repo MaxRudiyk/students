d40ec6db16051a141be2bae4956727124bf89240
Maksym <maxrudiyk@gmail.com>
2017-08-11 17:00:59 +0300

    Add group delete view(CBV)

--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -33,8 +33,8 @@ urlpatterns = [
     url(r'^groups/$', groups.groups_list, name='groups'),
     url(r'^groups/add/$', groups.groups_add, name='groups_add'),
     url(r'^groups/(?P<gid>\d+)/edit/$', groups.groups_edit, name='groups_edit'),
-    #url(r'^groups/(?P<pk>\d+)/delete/$', GroupDeleteView.as_view(), name='groups_delete'),
-    url(r'^groups/(?P<gid>\d+)/delete/$', groups.groups_delete, name='groups_delete'),
+    url(r'^groups/(?P<pk>\d+)/delete/$', GroupDeleteView.as_view(), name='groups_delete'),
+    #url(r'^groups/(?P<gid>\d+)/delete/$', groups.groups_delete, name='groups_delete'),
 
     # Exams urls
     url(r'^exams/$', exams.exams_list, name='exams'),
