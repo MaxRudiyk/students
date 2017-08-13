f8f180baf55d0bdf750868287f40c8d217a5dc18
Maksym <maxrudiyk@gmail.com>
2017-08-13 17:22:29 +0300

    add student copy func in admin actions

--- students/admin.py
+++ students/admin.py
@@ -4,11 +4,13 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from django.core.urlresolvers import reverse
 from .models import Student, Group, Exam, Result_exam
+from copy import deepcopy
 
 
 # Register your models here.
 
 class StudentAdmin(admin.ModelAdmin):
+    actions = ['copy_student']
     list_display = ['last_name', 'first_name', 'ticket', 'student_group']
     list_display_links = ['last_name', 'first_name']
     list_editable=['student_group']
@@ -20,7 +22,28 @@ class StudentAdmin(admin.ModelAdmin):
     def view_on_site(self, obj):
         return reverse('students_edit', kwargs={'pk': obj.id})
 
+    def copy_student(self, request, queryset):
+        old_obj = deepcopy(queryset)
+        for s in old_obj:
+            try:
+                s.pk = None
+                s.save()
+                self.message_user(request, 'Successfully copy students')
+            except Exception:
+                self.message_user(request, 'Copy is falied')
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ['title', 'leader']
+    list_display_links = ['title']
+    list_editable = ['leader']
+    ordering = ['title']
+    list_per_page = 10
+    search_fields = ['title', 'leader__last_name', 'leader__first_name']
+
+    def view_on_site(self, obj):
+        return reverse('groups_edit', kwargs={'pk': obj.id})
+
 admin.site.register(Student, StudentAdmin)
-admin.site.register(Group)
+admin.site.register(Group, GroupAdmin)
 admin.site.register(Exam)
 admin.site.register(Result_exam)
